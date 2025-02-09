73feb0e0f1945bc90f61b59b581d13ca14d0b1cb
beyazskorsky <hhsynalv@gmail.com>
2025-02-09 16:32:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const EmailService = require('./services/email.service');
+const path = require('path');
+
+const app = express();
+
+// Middleware'ler
+app.use(express.json());
+app.use(express.static('public'));
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 dakika
+    max: 10000 // IP başına maksimum istek
+});
+app.use('/send-email', limiter);
+
+// E-posta gönderim API'si
+app.post('/send-email', async (req, res) => {
+    const { email, type, data } = req.body;
+
+    try {
+        await EmailService.sendEmail(email, type, data);
+        res.status(200).json({ 
+            success: true, 
+            message: 'E-posta başarıyla gönderildi' 
+        });
+    } catch (error) {
+        res.status(400).json({ 
+            success: false, 
+            message: error.message 
+        });
+    }
+});
+
+// test sayfasını çekme yapıyoruz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Sunucuyu çalıştır
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
---
+++ templates/email.templates.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+// Helper function to read HTML templates dynamically
+const getHtmlTemplate = (templateName) => {
+    const templatePath = path.join(__dirname, `${templateName}.html`);
+    if (fs.existsSync(templatePath)) {
+        return fs.readFileSync(templatePath, 'utf8');
+    } else {
+        throw new Error(`Template ${templateName}.html not found`);
+    }
+};
+
+// Template functions
+const templates = {
+    welcome: (data) => {
+        const html = getHtmlTemplate('welcome-turkish'); 
+        return {
+            subject: 'Hoş Geldiniz!',
+            message: html.replace('{{username}}', data.name)
+            .replace('{{username}}', data.name),
+        };
+    },
+
+    announcement: (data) => {
+        const html = getHtmlTemplate('announcement'); 
+        return {
+            subject: `Sevgili ${data.name}, Yeni Bir Duyurumuz Var!`,
+            message: html
+                .replace('{{username}}', data.name)
+                .replace('{{message}}', data.message),
+        };
+    },
+
+    purchase: (data) => {
+        const html = getHtmlTemplate('purchase-turkish');
+        return {
+            subject: data.subject,
+            message: html
+                .replace('{{username}}', data.username)
+                .replace('{{title}}', data.title)
+                .replace('{{first}}', data.first)
+                .replace('{{credit_amount}}', data.credit_amount)
+                .replace('{{date}}', data.date),
+        };
+    },
+};
+
+module.exports = templates;
